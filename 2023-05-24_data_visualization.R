1b488544354a9edb0f93545bab33c28c5b23bed6
Kiran Patel <kapatel25@arizona.edu>
2023-05-24 23:30:12 +0000

    I did ggplot

--- 2023-05-24_data_visualization.R
+++ 2023-05-24_data_visualization.R
@@ -2,3 +2,46 @@
 # Kiran Patel
 # kapatel25@arizona.edu
 # 2023-05-24
+
+# installing packages
+install.packages("ggplot2")
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+install.packages("gapminder")
+library(gapminder)
+library(remotes)
+library(emoGG)
+
+# Visualizations ----------------------------------------------------------
+# Continuous (GDP)
+# ggplot(data = gapminder, 
+#        mapping = aes(x = gdpPercap, y = lifeExp)) + 
+#   geom_point(alpha = 0.5, color = "orange") + 
+# #  geom_emoji(emoji = "1f938") + 
+#   scale_x_log10() +
+#   geom_smooth(method = "lm", linewidth = 3, color = "black")
+
+# Categorical (year)
+ggplot(gapminder,
+       aes(x = year, y = lifeExp, group = country)) +
+  geom_point() +
+  geom_line(aes(color = continent))
+
+# Visualize Americas Only, using pipes (Ctrl + Shift + M)
+gapminder %>% 
+  filter(continent == "Americas") 
+
+Americas <- gapminder %>% 
+  filter(continent == "Americas")
+
+# Plot Americas
+ggplot(Americas,
+       aes(x = year, y = lifeExp, group = country)) +
+  geom_line() +
+  facet_wrap(~country) +
+  labs( x = "Year",
+        y = "Life Expectancy",
+        title = "Countries in the Americas")
+
+
